4ce131828679d403c341145fe7a8dd2ab92dafbe
ljuyx010 <34336224+ljuyx010@users.noreply.github.com>
2018-12-19 11:47:52 +0800

    20181219一测

--- application/index/controller/Logs.php
+++ application/index/controller/Logs.php
@@ -8,8 +8,8 @@ class Logs extends Common{
         $time=input('time')?strtotime(input('time')):mktime(0,0,0,date('m'),1,date('Y'));
         $user=db('admin')->where('zt',0)->select();
         if(session('user.type')){$uid=input('uid')?:session('user.id');}else{$uid=session('user.id');}
-        $list=db('logs')->alias('l')->join('admin a','l.uid=a.id')->join('admin m','l.to=m.id')->field('l.*,a.name,m.name as ton')
-        ->where('l.type',$t)->where('time','>= time',$time)->where('uid',$uid)->whereOr('to',$uid)->order('time desc')->select();
+        $list=db('logs')->alias('l')->join('admin a','l.uid=a.id')->join('admin m','l.to=m.id','LEFT')->field('l.*,a.name,m.name as ton')
+        ->where('l.type',$t)->where('time','>= time',$time)->where('uid|to',$uid)->order('time desc')->select();
         $this->assign('t',$t);
         $this->assign('u',$user);
         $this->assign('nid',$uid);
