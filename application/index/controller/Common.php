9a147a2ca04ef993249ea2bba8159ed232888de5
ljuyx010 <34336224+ljuyx010@users.noreply.github.com>
2018-12-19 09:53:20 +0800

    bug修复

--- application/index/controller/Common.php
+++ application/index/controller/Common.php
@@ -7,12 +7,14 @@ use think\Request;
 class Common extends Controller{
 	
 	public function _initialize(){
-		if(cookie('user')){
-			$rs=db('admin')->where('zt',0)->where('account',cookie('user.a'))->where('password',cookie('user.p'))->find();
-			session('user',$rs);
-		}
-		if(!session('user.id')){			
-			$this->redirect('login/index');
+		
+		if(!session('user.id')){
+			if(cookie('user')){
+				$rs=db('admin')->where('zt',0)->where('account',cookie('user.a'))->where('password',cookie('user.p'))->find();
+				session('user',$rs);
+			}else{
+				$this->redirect('login/index');
+			}			
 		}
     }
     
